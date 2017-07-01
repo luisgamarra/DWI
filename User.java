1f03fb5fd0269632bfe39a8f54b2103c449dbbc5
Lucero1308 <luuceroo01@gmail.com>
2017-07-01 10:37:20 -0500

    Add files via upload

---
+++ User.java
@@ -0,0 +1,132 @@
+package pe.edu.utp.BDWebApplication.models;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by Lucero on 14/06/2017.
+ */
+public class User {
+
+        private int id;
+        private int phone;
+        private String firstname;
+        private String lastname;
+        private String pass;
+        private String email;
+        private String photo;
+        private Account account;
+
+        public User() {
+        }
+
+        public User(int id, int phone, String firstname, String lastname, String pass, String email, String photo, Account account) {
+                this.id = id;
+                this.phone = phone;
+                this.setFirstname(firstname);
+                this.lastname = lastname;
+                this.pass = pass;
+                this.email = email;
+                this.photo = photo;
+                this.account = account;
+        }
+
+        public int getId() { return id; }
+
+        public String getIdAsValue() { return "'" + getId() + "'";}
+
+        public User setId(int id) {
+                this.id = id;
+                return this;
+        }
+
+        public int getPhone() {return phone; }
+
+        public String getPhoneAsValue() {
+                return "'" + getPhone() + "'";
+        }
+
+        public User setPhone(int phone) {
+                this.phone = phone;
+                return this;
+        }
+
+        public String getFirstname() {return firstname;}
+
+        public String getFirstnameAsValue() { return "'" + getFirstname() + "'";}
+
+        public User setFirstname(String firstname) {
+                this.firstname = firstname;
+                return this;
+        }
+
+        public String getLastname() {return lastname; }
+
+        public String getLastnameAsValue() {
+                return "'" + getLastname() + "'";
+        }
+
+        public User setLastname(String lastname) {
+                this.lastname = lastname;
+                return this;
+        }
+
+        public String getPass() {return pass; }
+
+        public String getPassAsValue() {
+                return "'" + getPass() + "'";
+        }
+
+        public User setPass(String pass) {
+                this.pass = pass;
+                return this;
+        }
+
+        public String getEmail() {return email; }
+
+        public String getEmailAsValue() {
+                return "'" + getEmail() + "'";
+        }
+
+        public User setEmail(String email) {
+                this.email = email;
+                return this;
+        }
+
+        public String getPhoto() {return photo; }
+
+        public String getPhotoAsValue() {
+                return "'" + getPhoto() + "'";
+        }
+
+        public User setPhoto(String photo) {
+                this.photo = photo;
+                return this;
+        }
+
+        public Account getAccount() {return account; }
+
+        public User setAccount(Account account) {
+                this.account = account;
+                return this;
+        }
+
+        public static User build(ResultSet rs,AccountsEntity accountsEntity) {
+                try {
+                        return (new User())
+                                .setId(rs.getInt("user_id"))
+                                .setFirstname(rs.getString("user_firstname"))
+                                .setLastname(rs.getString("user_lastname"))
+                                .setPass(rs.getString("user_pass"))
+                                .setPhone(rs.getInt("phone"))
+                                .setEmail(rs.getString("email"))
+                                .setPhoto(rs.getString("photo"))
+                                .setAccount(accountsEntity.findById(rs.getInt("account_id")));
+                } catch (SQLException e) {
+                        e.printStackTrace();
+                }
+                return null;
+        }
+
+
+}
